15a18fabe7b5bb1050c99eb7bda0482e041fda47
Tymon Wozniak <tymon.student@gmail.com>
2026-07-04 17:01:09 +0200

    explicitly ignoring error

--- cmd/lines/main.go
+++ cmd/lines/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	if err := run(os.Stdout, os.Stderr, os.Args); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
